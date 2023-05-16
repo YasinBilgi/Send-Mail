c3af8f719a2b367986989b34e0fdc71579d2a860
Yasin Bilgi <m.yasinbilgi03@gmail.com>
2023-05-17 01:03:39 +0300

    Send-Mail

---
+++ BirTakimisler/BirTakimisler/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Utility;
+
+MailSender.DefineSenderInformation("Gönderen Mail", "Gönderen Şifre");
+
+MailSender.SendMail("Gönderilen Mail", "Başlık", "Mesaj");
+
+Console.WriteLine("Mail gönderildi");
---
+++ BirTakimisler/Utility/MailSender.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class MailSender
+    {
+        private static string sendermail;
+        private static string senderpassword;
+
+        public static void DefineSenderInformation(string email, string password)
+        {
+            sendermail = email;
+            senderpassword = password;
+        }
+
+        public static void SendMail(string rocoiverMail, string subject, string messageBody)
+        {         
+            {
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    message.From = new MailAddress(sendermail);
+                    message.To.Add(new MailAddress(rocoiverMail));
+                    message.Subject = subject;
+                    message.IsBodyHtml = true;  
+                    message.Body = messageBody;
+                    smtp.Port = 587;
+                    smtp.Host = "smtp.office365.com";   
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(sendermail, senderpassword);
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Send(message);
+                }
+                catch (Exception ex) 
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
